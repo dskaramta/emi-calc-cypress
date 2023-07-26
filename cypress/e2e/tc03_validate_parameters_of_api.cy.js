1ee238e2ab4f8e73b2b5f6e2155da982349dff91
divyesh.karamta <Divyesh.Karamta@solarwinds.com>
2023-07-26 10:32:31 +0530

    api testcase completion

--- cypress/e2e/tc03_validate_parameters_of_api.cy.js
+++ cypress/e2e/tc03_validate_parameters_of_api.cy.js
@@ -2,12 +2,20 @@ describe('Automate API Test', () => {
     it('Validate the parameters of API', () => {
       cy.fixture('users.json').then((users) => {
         // Load users data from the fixture
+        const userArr = users.data 
   
         cy.request('GET', "https://reqres.in/api/users?page=2")
           .then((response) => {
             // Assertions on the response
             expect(response.status).to.eq(200);
-            expect(response.body.data).to.deep.eq(users.data)
+            const response_data = response.body.data
+            expect(response_data).to.deep.eq(users.data)
+            for(var i=0; i<userArr.length; i++){
+                expect(response_data[i]["id"]).to.eq(userArr[i]["id"])
+                expect(response_data[i]["email"]).to.eq(userArr[i]["email"])
+                expect(response_data[i]["first_name"]).to.eq(userArr[i]["first_name"])
+                expect(response_data[i]["last_name"]).to.eq(userArr[i]["last_name"])
+            }
           });
       });
     });
